25d6a698837ff2c0f4110b5e03a4253d3b946168
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2020-04-26 22:34:04 -0300

    Starting code about Red-Black Trees

---
+++ Data Structures/Trees/Red-Black Tree/Red-Black_Tree.c
@@ -0,0 +1,14 @@
+// This code is intended for people wishing to understand the Structure
+// of the Binary Tree. This createBinaryTree() function is merely didactic.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef double type;
+typedef char color;
+
+typedef struct binaryTree {
+	type value;
+  color nodeColor;
+	struct binaryTree * left, * right;
+} binTree;
