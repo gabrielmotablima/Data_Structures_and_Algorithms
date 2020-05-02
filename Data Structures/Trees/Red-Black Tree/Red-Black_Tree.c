c8f00a67f1985dd28b5e721a075dad6962232c9c
Gabriel Mota Bromonschenkel Lima <gabriel.mota.bromonschenkel.lima@hotmail.com>
2020-05-01 23:47:53 -0300

    Adding createRBTree() and main() blocks

--- Data Structures/Trees/Red-Black Tree/Red-Black_Tree.c
+++ Data Structures/Trees/Red-Black Tree/Red-Black_Tree.c
@@ -5,11 +5,33 @@
 #include <stdlib.h>
 
 typedef double type;
-typedef char color;
 
 typedef struct Tree {
 	type value;
-	color nodeColor;
+	char nodeColor;
 	struct Tree * left, * right;
-} RB_Tree;
+} RBTree;
 
+RBTree * createRBTree (RBTree * tree, type value, char color) {
+	if (tree == NULL) {
+		RBTree * newTree = (RBTree *) malloc (sizeof (RBTree));
+		
+		newTree -> value = value;
+		newTree -> nodeColor = color;
+		newTree -> right = NULL;
+		newTree -> left = NULL;
+		
+		return newTree;
+	}
+ 	else return tree;
+}
+
+int main () {
+   
+	RBTree * myTree = NULL;
+	type leafValue = 0;
+  	char color = 'r';
+	myTree = createRBTree( myTree,  leafValue, color );
+  
+	return 0;
+}
